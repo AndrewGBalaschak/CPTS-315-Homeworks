39fa6d445c08324578d5edd40ec2e7fbad1743e4
Avulsion <10779691+AndrewGBalaschak@users.noreply.github.com>
2023-02-09 17:18:23 -0800

    Added problem description

--- Homework 1/CPTS 315 HW 1 Basket Hashing.c
+++ Homework 1/CPTS 315 HW 1 Basket Hashing.c
@@ -1,4 +1,27 @@
-//(C) Andrew Balaschak, 2023
+//Code (C) Andrew Balaschak, 2023
+
+/*
+This question is about the PCY algorithm for counting frequent pairs of items. Suppose we have six items numbered 1, 2, 3, 4, 5, 6. Consider the following twelve baskets.
+    1.  {1,2,3}
+    2.  {2,3,4}
+    3.  {3,4,5}
+    4.  {4,5,6}
+    5.  {1,3,5}
+    6.  {2,4,6}
+    7.  {1,3,4}
+    8.  {2,4,5}
+    9.  {3,5,6}
+    10. {1,2,4}
+    11. {2,3,5}
+    12. {3,4,6}
+    
+Suppose the support threshold is 4. On the first pass of the PCY algorithm, we use a hash table with 11 buckets, and the set {i,j} is hashed to i × j mod 11.
+    a) By any method, compute the support for each item and each pair of items.
+    b) Which pairs hash to which buckets?
+    c) Which buckets are frequent?
+    d) Which pairs are counted on the second pass of the PCY algorithm?
+*/
+
 #include <stdio.h>
 
 int main(){
